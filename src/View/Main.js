4faa5c36f1d9b45e5ef8f2ed967645f9326492fa
velazmig3200 <82695995+velazmig3200@users.noreply.github.com>
2023-04-01 14:45:12 -0500

    restructured <Month />
    restructured Month component to handle each month individually and remove unrelated responsibilities in the component

--- src/View/Main.js
+++ src/View/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Month from "./Month";
 import viewCss from "./view.module.css";
 import WeekHeader from "./WeekHeader";
@@ -17,12 +17,97 @@ function Main({
 	const currentIndex = date.months.indexOf(month);
 	const numberDays = new Date(year, currentIndex + 1, 0).getDate();
 	const firstDay = date.days[new Date(year, currentIndex, 1).getDay()];
-	let scrollProp;
+	const monthContainer = useRef(null);
+	const currentMonth = useRef(null);
+	const spacer = useRef(null);
+	let visibleMonth;
+	let smoothScrollID;
+	let dxy = 25;
+
+	useEffect(() => {
+		clearInterval(smoothScrollID);
+		smoothScrollID = setInterval(smoothScroll, 1);
+	}, [navDate]);
+
+	async function monthContainerHeight() {
+		await new Promise(resolve => {
+			resolve(monthContainer.current);
+		});
+		const monthContainerTop = monthContainer.current.getBoundingClientRect().top;
+		monthContainer.current.style.height = `calc(100vh - ${monthContainerTop}px)`;
+	}
+	monthContainerHeight();
+
+	function smoothScroll() {
+		const container = monthContainer.current;
+		const containerRect = container.getBoundingClientRect();
+		const currentMonthRect = currentMonth.current.getBoundingClientRect();
+		const position = currentMonthRect.y - containerRect.top;
+
+		if (position > 1) {
+			monthContainer.current.scrollTop += dxy;
+			position / 40 < 1 && (dxy = position / 40 + 1);
+		} else if (position < 0) {
+			monthContainer.current.scrollTop -= dxy;
+			position / 40 > -1 && (dxy = position / 40 + 1);
+		} else {
+			clearInterval(smoothScrollID);
+			smoothScrollID = null;
+		}
+	}
+
+	function mouseWheel() {
+		const current = monthContainer.current;
+		const currentRect = current.getBoundingClientRect();
+		const scrollHeight = current.scrollHeight - currentRect.height;
+		const buffer = 20;
+
+		if (current.scrollTop < 1) {
+			setYear(year - 1);
+			setMonth(date.months[date.months.length - 1]);
+			current.scrollTop = scrollHeight - buffer;
+		} else if (current.scrollTop > scrollHeight - 1) {
+			setYear(year + 1);
+			setMonth(date.months[0]);
+			current.scrollTop = buffer;
+		}
+
+		const spacerRef = spacer.current.getBoundingClientRect().height;
+		const subdivision = Math.ceil((current.scrollTop - spacerRef * 1.3) / 290);
+		if (visibleMonth != subdivision) {
+			visibleMonth = subdivision;
+			visibleMonth < 0 && (visibleMonth = 0);
+			visibleMonth > 11 && (visibleMonth = 11);
+			setMonth(date.months[visibleMonth]);
+		}
+	}
+
+	const months = () => {
+		let result = [];
+		for (let i in date.months) {
+			result.push(
+				<Month
+					key={i}
+					currentMonth={month == date.months[i] ? currentMonth : null}
+					name={date.months[i]}
+				/>
+			);
+		}
+		return result;
+	};
 
 	return (
 		<div className={`${viewCss.viewContainer}`}>
 			<WeekHeader date={date} />
-			<Month
+			<main
+				ref={monthContainer}
+				onWheel={mouseWheel}
+				className={`${viewCss.monthContainer}`}>
+				<div ref={spacer} className={`${viewCss.spacer}`}></div>
+				{months()}
+				<div className={`${viewCss.spacer}`}></div>
+			</main>
+			{/* <Month
 				date={date}
 				month={month}
 				year={year}
@@ -30,7 +115,7 @@ function Main({
 				setMonth={setMonth}
 				navDate={navDate}
 				setNavDate={setNavDate}
-			/>
+			/> */}
 			{/* <p>
 				calendars:{" "}
 				<span style={{ color: "var(--debug-color" }}>{calendars.join(", ")}</span>
--- src/View/Month.js
+++ src/View/Month.js
@@ -1,85 +1,17 @@
-import React, { useEffect, useRef } from "react";
+import React, { useref } from "react";
 import viewCss from "./view.module.css";
 
-function Month({ date, month, year, setYear, setMonth, navDate }) {
-	let visibleMonth;
-	let smoothScrollID;
-	useEffect(() => {
-		clearInterval(smoothScrollID);
-		smoothScrollID = setInterval(smoothScroll, 1);
-	}, [navDate]);
-
-	const monthContainer = useRef(null);
-	const currentMonth = useRef(null);
-	let dxy = 25;
-
-	function smoothScroll() {
-		const monthRect = monthContainer.current.getBoundingClientRect();
-		const currentRect = currentMonth.current.getBoundingClientRect();
-		const position = currentRect.y - monthRect.top - 5;
-
-		if (position > 1) {
-			monthContainer.current.scrollTop += dxy;
-			position / 40 < 1 && (dxy = position / 40 + 1);
-		} else if (position < -1) {
-			monthContainer.current.scrollTop -= dxy;
-			position / 40 > -1 && (dxy = position / 40 + 1);
-		} else {
-			clearInterval(smoothScrollID);
-			smoothScrollID = null;
-		}
+function Month({ name, currentMonth }) {
+	async function getRef() {
+		await new Promise(resolve => {
+			resolve(currentMonth.current);
+		});
 	}
-
-	function mouseWheel() {
-		const containerCurrent = monthContainer.current;
-		const containerRect = containerCurrent.getBoundingClientRect();
-		const containerScrollHeight =
-			containerCurrent.scrollHeight - containerRect.height;
-		const buffer = 20;
-
-		if (containerCurrent.scrollTop < 1) {
-			setYear(year - 1);
-			setMonth(date.months[date.months.length - 1]);
-			containerCurrent.scrollTop = containerScrollHeight - buffer;
-		} else if (containerCurrent.scrollTop > containerScrollHeight - 1) {
-			setYear(year + 1);
-			setMonth(date.months[0]);
-			containerCurrent.scrollTop = buffer;
-		}
-
-		const containerSubdivision =
-			Math.ceil((containerCurrent.scrollTop - 450) / 290) - 1; //290
-		if (visibleMonth != containerSubdivision) {
-			visibleMonth = containerSubdivision;
-			visibleMonth < 0 && (visibleMonth = 0);
-			visibleMonth > 11 && (visibleMonth = 11);
-			setMonth(date.months[visibleMonth]);
-		}
-	}
-
-	const months = () => {
-		let months = [];
-		for (let i in date.months) {
-			months.push(
-				<div
-					key={i}
-					ref={month == date.months[i] ? currentMonth : null}
-					className={`${viewCss.month}`}>
-					{date.months[i]}
-				</div>
-			);
-		}
-		return months;
-	};
+	currentMonth && getRef();
 
 	return (
-		<div
-			ref={monthContainer}
-			onWheel={e => mouseWheel(e)}
-			className={`${viewCss.monthContainer}`}>
-			<div className={`${viewCss.spacer}`}></div>
-			{months()}
-			<div className={`${viewCss.spacer}`}></div>
+		<div ref={currentMonth} className={`${viewCss.month}`}>
+			{name}
 		</div>
 	);
 }
